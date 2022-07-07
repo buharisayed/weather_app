bec326896f90c4893c4521d7ec65643f78108bf0
Buhari Sayed <46406960+buharisayed@users.noreply.github.com>
2022-07-07 14:10:55 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import axios from "axios";
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=6160467e07839463580eb6305e88d861`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}℃</h1> : null}
+          </div>
+          <div className="description bold">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              <p>Feels like</p>
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}℃</p>
+              ) : null}
+            </div>
+            <div className="humidity">
+              <p>Humidity</p>
+              {data.main ? (
+                <p className="bold"> {data.main.humidity}%</p>
+              ) : null}
+            </div>
+            <div className="wind">
+              <p>Wind Speed</p>
+              {data.main ? (
+                <p className="bold"> {data.wind.speed}mph </p>
+              ) : null}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
